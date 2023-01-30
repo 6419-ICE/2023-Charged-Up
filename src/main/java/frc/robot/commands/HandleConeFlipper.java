0c409942de4e407f7e76860afc88609457fa862e
BuildTools <unconfigured@null.spigotmc.org>
2023-01-30 16:00:12 -0600

    Added comments to HandleConeFlipper and ConeFlipper classes

--- src/main/java/frc/robot/commands/HandleConeFlipper.java
+++ src/main/java/frc/robot/commands/HandleConeFlipper.java
@@ -12,31 +12,39 @@ public class HandleConeFlipper extends CommandBase{
     ConeFlipper coneFlipper = new ConeFlipper();
 
     private final ConeFlipper m_ConeFlipper;
+    // Constructor, used to pass in The ConeFlipper object to be used
     public HandleConeFlipper(ConeFlipper coneFlipper) {
         m_ConeFlipper = coneFlipper;
+        //Makes the ConeFlipper object required by the command
         addRequirements(m_ConeFlipper);
     }
 
     @Override
     public void end(boolean interrupted) {
+        //Stops motor once command ends
         coneFlipper.StopMotor();
         // TODO Auto-generated method stub
         super.end(interrupted);
     }
-
+//Loops while command is running 
     @Override
     public void execute() {
+        //checks for button press
         if (RobotContainer.GetConeFlipperUpButton()) {
+            //sets the motor to move up
             coneFlipper.MoveUp();
-           // ConeFlipper.set(ControlMode.PercentOutput, -0.5);
+            // ConeFlipper.set(ControlMode.PercentOutput, -0.5);
       } 
+      //checks for button press
      else if (RobotContainer.GetConeFlipperDownButton()) {
+        //sets motor to move down
         coneFlipper.MoveDown();
         //ConeFlipper.set(VictorSPXControlMode.PercentOutput, 0.5);   
       } else {
+        //Stops when no button pressed
         coneFlipper.StopMotor();
       }
-    
+    //makes up have priority (if both are pressed the motor goes up)
       if (RobotContainer.GetConeFlipperDownButton() && RobotContainer.GetConeFlipperUpButton()) {
         coneFlipper.MoveUp();
       }
