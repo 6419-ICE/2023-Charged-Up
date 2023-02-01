5f842b58a2e0cb8cc3b9f2f582a33d582ec6d542
LeTegu <107449459+LeTegu@users.noreply.github.com>
2023-02-01 14:47:57 -0600

    2/1/2023
    Added the charge station auto and the command to run it, however the command does not work out

--- src/main/java/frc/robot/commands/BalanceGyroCommand.java
+++ src/main/java/frc/robot/commands/BalanceGyroCommand.java
@@ -8,7 +8,34 @@ import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.Constants.DriveConstants;
 import frc.robot.Constants.AutoConstants;
+import edu.wpi.first.wpilibj.ADIS16470_IMU;
 
+// This command moves the robot to find balance with the gyro
 public class BalanceGyroCommand extends CommandBase {
+    private ADIS16470_IMU m_ADIS16470;
+    private DriveSubsystem m_driveSubSystem;
     
+      // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+   
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return super.isFinished();
+  }
 }
